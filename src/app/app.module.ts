f708b932a79af075f41f6fd31520edf5ddba876a
nsdigirolamo <nsdigirolamo@gmail.com>
2023-07-06 16:02:48 -0400

    Resolved merge conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppQuizComponent } from './app-quiz/app-quiz.component';
-<<<<<<< HEAD
-=======
 import { TopbannerComponent } from './topbanner/topbanner.component';
 import { ChatboxComponent } from './chatbox/chatbox.component';
->>>>>>> 6891909c4b050c4e9a40f685139217c97b519bd5
 
 @NgModule({
   declarations: [
     AppComponent,
     ChatboxComponent,
     AppQuizComponent,
-<<<<<<< HEAD
-=======
-    ChatboxComponent,
     TopbannerComponent
->>>>>>> 6891909c4b050c4e9a40f685139217c97b519bd5
   ],
   imports: [
     BrowserModule
