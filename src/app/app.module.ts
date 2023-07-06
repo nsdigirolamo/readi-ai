66ffc08e5f7cab23493acd925c6a0e572c0b4a0e
Siraj Mughal <smm24@njit.edu>
2023-07-06 15:44:37 -0400

    updated format

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppQuizComponent } from './app-quiz/app-quiz.component';
-import { ChatboxComponent } from './chatbox/chatbox.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppQuizComponent,
-    ChatboxComponent
   ],
   imports: [
     BrowserModule
