ac96449b7812d67cc359dea60abc457fc28ab432
Siraj Mughal <smm24@njit.edu>
2023-07-07 10:26:57 -0400

    Quiz Question Verification

--- src/app/app-quiz/app-quiz.component.ts
+++ src/app/app-quiz/app-quiz.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app-quiz.component.css']
 })
 export class AppQuizComponent {
+  isButtonClicked = [false, false, false];
+
+
+  toggleButton(index: number) {
+    this.isButtonClicked[index] = !this.isButtonClicked[index];
+  }
+
+  getButtonStyle(index: number) {
+    if (this.isButtonClicked[index]) {
+
+        return { 'background-color': 'red' };
+      
+    }
+    else {
+      return { 'background-color': 'red' };
+    }
+    return null;
+  }
 
 }
+
+
